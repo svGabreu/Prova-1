17c5ece615aa1b103853f7f5e727c5eb2ac70074
Gabriell Souza Viana <gabriellsamy08@gmail.com>
2024-10-01 20:38:14 -0300

    Questão 3

--- setup/urls.py
+++ setup/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#]
+
+from todos.views import home
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+      path("admin/", admin.site.urls),
+      path(" ", home), 
+] 
