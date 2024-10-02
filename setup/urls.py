3030017cb17fcdb8a9f99011907c7c297652afa7
Gabriell Souza Viana <gabriellsamy08@gmail.com>
2024-10-01 21:11:59 -0300

    Questões 4 e 5

--- setup/urls.py
+++ setup/urls.py
@@ -16,13 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-#urlpatterns = [
-#    path('admin/', admin.site.urls),
-#]
-
 from todos.views import home
+
 urlpatterns = [
-      path("admin/", admin.site.urls),
-      path(" ", home), 
+      path("", home), 
 ] 
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+
 def home(request):
-    return HttpResponse('todos/home.html')
+    return render(request, "todos/home.html");
 
